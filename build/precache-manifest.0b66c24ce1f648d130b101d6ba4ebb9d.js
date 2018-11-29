d90b99130c35f0800926fa8dd062181a1618a50e
Wayne Cheng <wayne@che.ng>
2018-11-29 01:08:44 -0800

    added build files

---
+++ build/precache-manifest.0b66c24ce1f648d130b101d6ba4ebb9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ed7d5cfb08da6522d3f",
+    "url": "/static/js/main.0ed7d5cf.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "0ed7d5cfb08da6522d3f",
+    "url": "/static/css/main.afb12a3d.chunk.css"
+  },
+  {
+    "revision": "d4539075b761b2fcefe4a015b7d88e41",
+    "url": "/index.html"
+  }
+];
