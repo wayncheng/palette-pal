8d484f19a5e0a5ce11279a222dccc192dedcbb53
Wayne Cheng <wayne@che.ng>
2018-11-29 02:00:34 -0800

    Updates

---
+++ precache-manifest.3304eb6645fdb45c10bd56e3c2209b73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea6b86b69c71e693d01",
+    "url": "/palette-pal/static/js/runtime~main.9ea6b86b.js"
+  },
+  {
+    "revision": "add49b3c7ff4b6fcd34e",
+    "url": "/palette-pal/static/js/main.add49b3c.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/palette-pal/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "add49b3c7ff4b6fcd34e",
+    "url": "/palette-pal/static/css/main.afb12a3d.chunk.css"
+  },
+  {
+    "revision": "1c0a6a5225e8555cc128047a684753f8",
+    "url": "/palette-pal/index.html"
+  }
+];
